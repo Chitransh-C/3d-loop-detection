7963d9217fee8e4649dab0d1599a6850776a9bce
Chitransh Chocksey <chitransh33333@gmail.com>
2025-05-04 14:36:30 +0530

    Delete nodes.js

--- nodes.js
+++
@@ -1,7 +0,0 @@
-// Linked list nodes with a loop: 0 -> 1 -> 2 -> 3 -> 1
-export const NODES = [
-  { id: 0, next: 1 },
-  { id: 1, next: 2 },
-  { id: 2, next: 3 },
-  { id: 3, next: 1 }  // loop back to 1
-];
